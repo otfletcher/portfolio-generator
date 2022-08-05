6a748657091e78baa81b1a225187eb24ba046816
Olivia <oliviatfletcher@outlook.com>
2022-08-05 16:04:31 -0700

    9.3.7

--- app.js
+++ app.js
@@ -6,18 +6,47 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
-      },
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please eneter your name!');
+                return false;
+            }
+            }
+        },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please eneter your Github username!');
+                return false;
+            }
+            }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter information about yourself for an "About" section?',
+        default: 'true'
       },
       {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
-      }
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+            }
+        }
     ]);
   };
 
